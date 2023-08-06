97b7d2ebaca5febf6d3fdebb5ef49905c2b5704a
xuchg1 <xuchg1@chinatelecom.cn>
2023-08-06 11:37:11 +0800

    <add> 复习knn算法

--- lesson5/KNN识别手写数字/code/IdentifImg.py
+++ lesson5/KNN识别手写数字/code/IdentifImg.py
@@ -5,7 +5,7 @@ Created on 2020年1月11日
 '''
 
 import os
-from com.msb.knn.KNNDateOnHand import *
+from KNNDateOnHand import *
 
 # 将每一个文件的内容拼接成一行
 def img2vector(filename):
@@ -22,26 +22,34 @@ def IdentifImgClassTest():
     # 分类号（数字）
     hwLabels = []
     # 获取TrainData目录下所有的文件
-    trainingFileList = os.listdir('../../../data/TrainData')
-    # 训练集的数据量，空间中分布的数据量  m=1934
+    # os 模块提供了非常丰富的方法用来处理文件和目录
+    # os.listdir(path) 返回path指定的文件夹包含的文件或文件夹的名字的列表
+    trainingFileList = os.listdir('../data/TrainData')
+    # 训练集的数据量，空间中分布的数据量  m=1934, 1934个文件
     m = len(trainingFileList)
     # 1934*1024的零矩阵
+    # 构建一个m行1024列的零矩阵，用于存储所有的训练数据
+    # 为什么是1024 列，因为每个文件中有32行，每行有32个字符，所以每个文件有1024个字符
     trainingMat = np.zeros((m, 1024))
     for i in range(m):
         fileNameStr = trainingFileList[i]
+        # 以'.'为分隔符，将文件名分割为两部分，取第一部分
         fileStr = fileNameStr.split('.')[0]
+        # 以'_'为分隔符，将文件名分割为两部分，取第一部分
         classNumStr = int(fileStr.split('_')[0])
+        # 将分类号添加到hwLabels中
         hwLabels.append(classNumStr)
-        trainingMat[i, :] = img2vector('../../../data/TrainData/%s' % fileNameStr)
+        # 将训练集数据存储到trainingMat矩阵中
+        trainingMat[i, :] = img2vector('../data/TrainData/%s' % fileNameStr)
 
-    testFileList = os.listdir('../../../data/TestData')
+    testFileList = os.listdir('../data/TestData')
     errorCount = 0.0
     mTest = len(testFileList)
     for i in range(mTest):
         fileNameStr = testFileList[i]
         fileStr = fileNameStr.split('.')[0]
         classNumStr = int(fileStr.split('_')[0])
-        vectorUnderTest = img2vector('../../../data/TestData/%s' % fileNameStr)
+        vectorUnderTest = img2vector('../data/TestData/%s' % fileNameStr)
         # 将训练集数据和第一条测试数据 以及K值传递给classify这个方法
         classifierResult = classify(vectorUnderTest, trainingMat, hwLabels, 3)
         print("识别出的数字是: %d, 真实数字是: %d" % (classifierResult, classNumStr))
--- lesson5/KNN识别手写数字/code/KNNDateByScikit-learn.py
+++ lesson5/KNN识别手写数字/code/KNNDateByScikit-learn.py
@@ -4,20 +4,23 @@ Created on 2020年1月11日
 
 @author: root
 '''
-from sklearn.neighbors.unsupervised import NearestNeighbors
+from sklearn.neighbors._unsupervised import NearestNeighbors
 import numpy as np
-from com.msb.knn.KNNDateOnHand import *
+from KNNDateOnHand import *
 
-datingDataMat, datingLabels = file2matrix('../../../data/datingTestSet2.txt')
+datingDataMat, datingLabels = file2matrix('../../../lesson4/data/datingTestSet2.txt')
 normMat, ranges, minVals = autoNorm(datingDataMat)
 
+# 1.使用sklearn中的KNN算法, NearestNeighbors 最近邻算法, n_neighbors=10, 代表的是取最近的10个点, 默认是5个点, fit()方法是归一化后的训练模型
 nbrs = NearestNeighbors(n_neighbors=10).fit(normMat)
 input_man = [[50000, 8, 9.5]]
+# 2.使用kneighbors()方法, 返回的是距离和索引, distances是距离, indices是索引
 S = (input_man - minVals) / ranges
 distances, indices = nbrs.kneighbors(S)
 # classCount   K：类别名    V：这个类别中的样本出现的次数
 
 classCount = {}
+# 从最近的10个点中, 找出出现次数最多的类别
 for i in range(10):
     voteLabel = datingLabels[indices[0][i]]
     classCount[voteLabel] = classCount.get(voteLabel, 0) + 1
